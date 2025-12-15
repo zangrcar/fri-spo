cceb6d54288a1c207a17b8bd5ea726785994fe1a
Žan Grčar <grcar.zan1@gmail.com>
2025-12-15 13:07:26 +0100

    Third project: assembler

--- assembler/test.asm
+++ assembler/test.asm
@@ -1,8 +1,11 @@
 TEST    START   1000
 FIRST   LDA     ALPHA
         LDB     #3
+        BASE    ALPHA
         ADDR    A, B
-        +STB     BETA
+        +STB    BETA
+        LDA     BETA
+        NOBASE
 ALPHA   WORD    1
 BETA    RESW    2
         END     FIRST
